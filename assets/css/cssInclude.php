fdee5da037278fd8302538b557d38daebf274bf9
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-10 20:51:11 -0300

    Criado e estilizado tela de login.

--- assets/css/cssInclude.php
+++ assets/css/cssInclude.php
@@ -1,4 +1,5 @@
 <?php
 // aqui eu vou utilizar para chamar as libs css em todas as telas.
-echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">';
+echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />';
 ?>
--- view/login.php
+++ view/login.php
@@ -6,10 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <?php include "../assets/css/cssInclude.php"; ?>
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
-<body>
+<body >
     <div class="container">
-        <h1>Login</h1>
+        <div class="jumbotron  loginConteiner center" >
+            <h2><i class="fas fa-vial"></i> Bem vindo ao meu teste</h2>
+            <p><i class="fa-solid fa-briefcase"></i>: Full Stack PHP Pleno</p>
+            <form>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa-solid fa-envelope"></i></span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa-solid fa-key"></i></span>
+                    </div>
+                    <input type="password" class="form-control" placeholder="Senha">
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-success btn-block"><i class="fa-solid fa-door-open"></i> Entrar</button>
+                    </div>
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary btn-block"><i class="fa-solid fa-face-frown"></i> Esqueci minha senha</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
     <?php include "../assets/js/jsInclude.php"; ?>
