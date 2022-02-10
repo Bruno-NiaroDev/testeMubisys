ee8c16eff59498d945374ce29ee4ae92b378cf19
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-10 17:41:35 -0300

    Estruturado controlador de usuario.

--- controller/usuario.php
+++ controller/usuario.php
@@ -1,3 +1,17 @@
 <?php
-//controller
+
+switch($_GET['acao']){
+    case 'logar': 
+        // Fazer o paranaue de logar aqui
+        break;
+    case 'cadastrar':
+        // Cadastrar o usuário aqui
+        break;
+    case 'listar': 
+        //listar o usuário aqui
+        break;
+    default: 
+        //configurar pagina de erro pra isso
+        echo 'Tivemos um probleminha aqui';
+}
 ?>
