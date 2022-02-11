91dd30c099f3fe6c28aa6075560f6eed96de401a
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 03:16:14 -0300

    Isolando o menu como componente para ser utilizado nas outras telas.

---
+++ view/component/navbar.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light bg-info navStyle">
+    <span class="navbar-brand">Bem Vindo <b><?php echo $_SESSION['dadosUsuario']['nome']; ?></b></span>
+    <div class="form-inline">
+        <a href="../index.php" ><i class="fa-solid fa-right-from-bracket"></i> Sair</a>
+    </div>
+</nav>
