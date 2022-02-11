f661d0ddb0328768b0e65db9e3ba04c07fd6b18c
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 00:30:12 -0300

    Decidi fazer a listagem sem a extenção do DataTable.

--- assets/css/cssInclude.php
+++ assets/css/cssInclude.php
@@ -2,6 +2,5 @@
 // aqui eu vou utilizar para chamar as libs css em todas as telas.
 echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">'
 .'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />'
-.'<link href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" rel="stylesheet">'
 .'<link href="../assets/css/style.css" rel="stylesheet">';
 ?>
