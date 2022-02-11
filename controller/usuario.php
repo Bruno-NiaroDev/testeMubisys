1c58ed9b019c331195d832534cc98fb7bd87979c
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 02:23:52 -0300

    Implementado chamada do metodo de login no controller de usuario.

--- controller/usuario.php
+++ controller/usuario.php
@@ -1,14 +1,29 @@
 <?php
 
+include '../database/config.php';
+
+include '../model/usuario.php';
+
+$database = new Database;
+
+$db = $database->getConnection();
+
+$usuario = new Usuario($db);
+
 switch($_GET['acao']){
     case 'logar': 
-        // Fazer o paranaue de logar aqui
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        if($usuario->checkLogin($email,$senha)){
+            header('Location: ../view/usuario.php');
+        } else {
+            header('Location: ../view/login.php?erro=1');
+        }
+
         break;
     case 'cadastrar':
         // Cadastrar o usuário aqui
-        break;
-    case 'listar': 
-        //listar o usuário aqui
         break;
     default: 
         //configurar pagina de erro pra isso
