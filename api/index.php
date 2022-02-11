e1cefde38ab3e93f33c5b5234a3d371ff84d9403
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 11:46:34 -0300

    Criado o endpoint api/usuarios

--- api/index.php
+++ api/index.php
@@ -1,3 +1,32 @@
 <?php
-// Apenas deixando criado.
+
+include '../database/config.php';
+include '../model/usuario.php';
+
+$database = new Database;
+
+$db = $database->getConnection();
+
+$usuario = new Usuario($db);
+
+if($_GET['rota'] == "usuarios"){
+    $headerParm = getallheaders();
+
+    $requestToken = $headerParm['token'];
+
+    if(!empty($requestToken) && $usuario->validaToken($requestToken)){
+        echo json_encode($usuario->listar());
+    } else {
+        echo json_encode([
+            'code' => 401,
+            'message' => 'Token inválido'
+        ]);   
+    }
+} else {
+    echo json_encode([
+        'code' => 400,
+        'message' => 'Ops! tivemos um problema para atender a sua requisição.'
+    ]);
+}
+
 ?>
