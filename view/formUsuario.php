b1ce8ad951f6b8beb68583e50c4f86bb5d8f1f07
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 03:17:39 -0300

    Implementado formulário de usuário.

--- view/formUsuario.php
+++ view/formUsuario.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+
+    if(!isset($_SESSION['dadosUsuario'])){
+        header('Location: ../view/login.php');
+    }
+?>  
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +15,23 @@
     <?php include "../assets/css/cssInclude.php"; ?>
 </head>
 <body>
+    <?php include './component/navbar.php'; ?>
     <div class="container">
-        <h1>Formulário</h1>
+        <br>
+        <div class="alert alert-danger" id="alertError" style="display: none">
+            <strong>Ops!</strong> <span id="msgErro"></span>
+        </div>
+        <div class="alert alert-success" id="alertSuccess" style="display: none">
+            <strong>Parabens!</strong> <span id="msgSucess"></span>
+        </div>
+        <div class="row">
+            <div class="col-sm-8">
+                <h2>Cadastro de Usuário</h2>
+            </div>
+            <div class="col-sm-4">
+                <a href="usuario.php" class="btn btn-primary btn-block"><i class="fa-brands fa-searchengin"></i> Voltar para Consulta</a>
+            </div>
+        </div>
     </div>
 
     <?php include "../assets/js/jsInclude.php"; ?>
