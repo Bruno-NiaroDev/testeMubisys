d5a1775f5b398bd219b145782c0db64e4cb84dc1
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 03:57:15 -0300

    fixed bug model.

--- model/usuario.php
+++ model/usuario.php
@@ -111,8 +111,6 @@ class Usuario {
 
                 return true;
             }else{
-                echo "nenhum registro.";
-                die();
                 return false;
             }
             
