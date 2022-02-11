ede72f36861977752b2eefe61e9e0f6fa6698a08
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-11 00:31:30 -0300

    Definido layout da listagem de usuários.

--- view/usuario.php
+++ view/usuario.php
@@ -8,9 +8,42 @@
     <?php include "../assets/css/cssInclude.php"; ?>
 </head>
 <body>
+    <nav class="navbar navbar-expand-sm bg-info navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <span class="navbar-brand">Teste do Bruno</span>
+            </li>
+        </ul>
+    </nav>
     <div class="container">
-        <h1>Usuario</h1>
-    </div>
+        <br>
+        <div class="alert alert-danger" id="alertError" style="display: none">
+            <strong>Ops!</strong> <span id="msgErro"></span>
+        </div>
+        <div class="alert alert-success" id="alertSuccess" style="display: none">
+            <strong>Parabens!</strong> <span id="msgSucess"></span>
+        </div>
+        <h2>Usuários</h2>
+        <table class="table">
+            <thead class="thead-light">
+                <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>-</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Bruno Araujo</td>
+                    <td>bruno.absilva3@gmail.com</td>
+                    <td> 
+                    <a href="formUsuario.php?id=123">Ver</a>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+     </div>
 
     <?php include "../assets/js/jsInclude.php"; ?>
 </body>
